6fa12a26ee66a785330264637fbf6a060bfdceca
CristianP_1278_W <c.terry009@hotmail.com>
2024-02-20 22:28:25 -0500

    Added files to the repository

--- 02-errorsfpnumbers/Diferencias_numéricas_en_sumas.cpp
+++ 02-errorsfpnumbers/Diferencias_numéricas_en_sumas.cpp
@@ -4,45 +4,49 @@
 
 using namespace std;
 
-double Sum1(int N){
+float Sum1(int N){
     
-    double sum1 = 0.0;
+    float sum1 = 0.0;
 
-    for (float k = 1; k <N; ++k){
-        sum1 += sum1 + 1/k;
+    for (float k = 1; k <=N; ++k){
+        sum1 = sum1 + 1/k;
         //cout << "Sum2: " << sum2 << endl;
     }
     return sum1;
 }
 
-double Sum2(int N){
+float Sum2(int N){
     
-    double sum2 = 0.0;
+    float sum2 = 0.0;
 
     for (float k = N; k >= 1; --k){
-        sum2 += sum2 + 1/k;
+        sum2 = sum2 + 1/k;
         //cout << "Sum2: " << sum2 << endl;
     }
     return sum2;
     
 }
 
-double RelativeDifference(double S1, double S2){
+float RelativeDifference(float S1, float S2){
     return abs(1-S1/S2);
 }
 
 
 int main() {
 
-    int N = 10000;
+    int N = 1e6;
     
-    cout <<"N"<< "\t"<<"S1"<<"\t"<<"S2"<<"\t"<<"Delta"<<endl;
+    cout <<"N"<<"\t"<<"S1"<<"\t"<<"S2"<<"\t"<<"Delta"<<endl;
 
-    for (int i = 1; i <= N; ++i){
-        double S1 =+ Sum1(i);
-        double S2 =+ Sum2(i);
-        double delta = RelativeDifference(S1, S2);
-        cout << i << "\t" << S1 << "\t" << S2 << "\t" << delta << endl;
+    for (int i = 1; i < N; ++i){
+        float S1 = Sum1(i);
+        float S2 = Sum2(i);
+        float delta = abs(1-S1/S2);
+        //float delta = RelativeDifference(S1, S2);
+        if (i%1000 == 0){
+            cout << i << "\t" << S1 << "\t" << S2 << "\t" << delta << endl;
+        }
+        
     }
 
     return 0;
